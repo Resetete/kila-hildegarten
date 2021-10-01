098ebd91898d73ff3f14ca20d521c796e9d8ff6a
Theresa <tmannschatz@gmail.com>
2021-10-01 23:23:37 +0200

    About us page content

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,8 @@ class PagesController < ApplicationController
     @jobs = Content.find_by(page: 'Stellenausschreibung')
     @kila_free_positions = Content.find_by(page: 'Freie Plätze')
   end
+
+  def about_us
+    @main_content = Content.find_by(page: 'Wir über uns')
+  end
 end
