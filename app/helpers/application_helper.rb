aea3b4a786168353d5ce60d60444fe65b13e89ef
Theresa <tmannschatz@gmail.com>
2021-10-02 22:48:00 +0200

    Add helper method for content editing actions; some home page styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def show_content_actions(page)
+    if signed_in?
+      [
+        (link_to 'new', new_content_path, class: 'waves-effect waves-light btn-small'),
+        (link_to 'edit', edit_content_path(page), class: 'waves-effect waves-light btn-small')
+      ].join(' ').html_safe
+    end
+  end
 end
