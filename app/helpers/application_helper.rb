1f9559b52c359e17638aaa0ab7b21f90fda5d334
Theresa <tmannschatz@gmail.com>
2021-10-04 13:14:28 +0200

    Refactor content action buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,20 @@
 module ApplicationHelper
   def show_content_actions(page)
     if signed_in?
+      button_types(page).join(' ').html_safe
+    end
+  end
+
+  private
+
+  def button_types(page)
+    if Content.find_by(page: page)
+      (link_to 'new', new_content_path, class: 'waves-effect waves-light btn-small')
+    else
       [
-        (link_to 'new', new_content_path, class: 'waves-effect waves-light btn-small'),
-        (link_to 'edit', edit_content_path(page), class: 'waves-effect waves-light btn-small')
-      ].join(' ').html_safe
+        (link_to 'edit', edit_content_path(page), class: 'waves-effect waves-light btn-small'),
+        (link_to 'delete', content_path(page), method: :delete, class: 'waves-effect waves-light btn-small')
+      ]
     end
   end
 end
