7809f1d2e69d710c17f4c10dabf326349322aa1f
Theresa <tmannschatz@gmail.com>
2021-10-10 22:02:13 +0200

    Form styling

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,4 @@
 class Page < ApplicationRecord
-  # has_many :images --> I don't have a pages table
-
+  # TODO: can be removed when pages passed as arguments into form link url
   PAGE_TYPES = ['Home', 'Stellenausschreibung', 'Freie Plätze', 'Kinderladen', 'Wir über uns', 'Tagesablauf', 'Räume', 'Konzept', 'Eingewöhnung', 'Team', 'Eltern', 'Kontakt']
 end
