2e0992b5da9cb46223134ea0940946d9a532c325
Theresa <tmannschatz@gmail.com>
2021-11-29 15:05:28 +0100

    test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,4 @@
 CarrierWave.configure do |config|
-  # config.dropbox_access_token = Rails.application.credentials.dropbox[:access_token]
-  config.dropbox_access_token = ENV['DROPBOX_ACCESS_TOKEN']
+  config.dropbox_access_token = Rails.application.credentials.dropbox_access_token
+  #config.dropbox_access_token = ENV['DROPBOX_ACCESS_TOKEN']
 end
