b056659a72f90751bd88a3d0d7caeb639cc3880f
Theresa <tmannschatz@gmail.com>
2021-12-06 13:43:31 +0100

    Rename page names

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,18 +3,18 @@ class PagesController < ApplicationController
     # TODO: refactor --> maybe looping over array with page names
     # TODO: improve page names
     # TODO: reduce image size --> image uploader
-    @main_content = Content.find_by(page: 'Home')
-    @jobs = Content.find_by(page: 'Stellenausschreibung')
-    @kila_free_positions = Content.find_by(page: 'Freie Plätze')
-    @about_us = Content.find_by(page: 'Über uns')
-    @daily_life_intro = Content.find_by(page: 'Tagesablauf Einführung')
-    @daily_life = Content.find_by(page: 'Tagesablauf')
+    @main_content = Content.find_by(page: 'home')
+    @jobs = Content.find_by(page: 'open_positions')
+    @kila_free_positions = Content.find_by(page: 'open_places')
+    @about_us = Content.find_by(page: 'about_us')
+    @daily_life_intro = Content.find_by(page: 'daily_life_intro')
+    @daily_life = Content.find_by(page: 'daily_life')
 
-    @images = Image.where(page: 'Home').last(Image::MAX_IMAGES_ON_HOMEPAGE) # max number of images shown on home page
-    @daily_life_images = Image.where(page: 'daily_life_image').last(Image::MAX_IMAGES_ON_HOMEPAGE)
+    @images = Image.where(page: 'home').last(Image::MAX_IMAGES_ON_HOMEPAGE) # max number of images shown on home page
+    @daily_life_images = Image.where(page: 'daily_life').last(Image::MAX_IMAGES_ON_HOMEPAGE)
   end
 
   def contact
-    @main_content = Content.find_by(page: 'Kontakt')
+    @main_content = Content.find_by(page: 'contact')
   end
 end
