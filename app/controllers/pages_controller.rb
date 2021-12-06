a152b5f90dc0e932d35865c7ba31b1488c257528
Theresa <tmannschatz@gmail.com>
2021-12-06 14:21:51 +0100

    Refactor pages controller to dynamically generate variables

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,15 +2,10 @@ class PagesController < ApplicationController
   def home
     # TODO: refactor --> maybe looping over array with page names
     # TODO: reduce image size --> image uploader
-    @main_content = Content.find_by(page: 'home')
-    @jobs = Content.find_by(page: 'open_positions')
-    @kila_free_positions = Content.find_by(page: 'open_places')
-    @about_us = Content.find_by(page: 'about_us')
-    @daily_life_intro = Content.find_by(page: 'daily_life_intro')
-    @daily_life = Content.find_by(page: 'daily_life')
-    @rooms = Content.find_by(page: 'rooms')
-    @concept = Content.find_by(page: 'concept')
-    @familarization = Content.find_by(page: 'familarization')
+
+    Page::NAMES.each do |page_name|
+      instance_variable_set "@#{page_name}", page_content_retriever(page_name)
+    end
 
     @home_images = Image.where(page: 'home').last(Image::MAX_IMAGES_ON_HOMEPAGE) # max number of images shown on home page
     @daily_life_images = Image.where(page: 'daily_life').last(Image::MAX_IMAGES_ON_HOMEPAGE)
@@ -19,5 +14,11 @@ class PagesController < ApplicationController
 
   def contact
     @main_content = Content.find_by(page: 'contact')
+  end
+
+  private
+
+  def page_content_retriever(page)
+    Content.find_by(page: page)
   end
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,3 @@
 class Page < ApplicationRecord
+  NAMES = %w[home open_positions open_places about_us daily_life daily_life_intro rooms concept familarization]
 end
