1196b4d194212482b02e9f6379324f01371b55dd
Theresa <tmannschatz@gmail.com>
2021-12-06 14:28:19 +0100

    Refactor pages controller - images variables

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,10 @@
 class PagesController < ApplicationController
   def home
-    # TODO: refactor --> maybe looping over array with page names
     # TODO: reduce image size --> image uploader
-
     Page::NAMES.each do |page_name|
       instance_variable_set "@#{page_name}", page_content_retriever(page_name)
+      instance_variable_set "@#{page_name}_images", page_image_retriever(page_name)
     end
-
-    @home_images = Image.where(page: 'home').last(Image::MAX_IMAGES_ON_HOMEPAGE) # max number of images shown on home page
-    @daily_life_images = Image.where(page: 'daily_life').last(Image::MAX_IMAGES_ON_HOMEPAGE)
-    @rooms_images = Image.where(page: 'rooms')
   end
 
   def contact
@@ -20,5 +15,9 @@ class PagesController < ApplicationController
 
   def page_content_retriever(page)
     Content.find_by(page: page)
+  end
+
+  def page_image_retriever(page)
+    Image.where(page: page)
   end
 end
