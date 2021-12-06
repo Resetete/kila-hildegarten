bcf7199b5443c48b662627c5b3a520a3bc719198
Theresa <tmannschatz@gmail.com>
2021-12-06 13:07:47 +0100

    Add daily life section on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,17 @@
 class PagesController < ApplicationController
   def home
+    # TODO: refactor --> maybe looping over array with page names
+    # TODO: improve page names
+    # TODO: reduce image size --> image uploader
     @main_content = Content.find_by(page: 'Home')
     @jobs = Content.find_by(page: 'Stellenausschreibung')
     @kila_free_positions = Content.find_by(page: 'Freie Plätze')
     @about_us = Content.find_by(page: 'Über uns')
+    @daily_life_intro = Content.find_by(page: 'Tagesablauf Einführung')
+    @daily_life = Content.find_by(page: 'Tagesablauf')
 
     @images = Image.where(page: 'Home').last(Image::MAX_IMAGES_ON_HOMEPAGE) # max number of images shown on home page
+    @daily_life_images = Image.where(page: 'daily_life_image').last(Image::MAX_IMAGES_ON_HOMEPAGE)
   end
 
   def contact
