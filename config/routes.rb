3d1854a552d52802d2408776d17210775c800a9c
Theresa <tmannschatz@gmail.com>
2021-12-06 15:03:28 +0100

    Add footer

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :admins
   root 'pages#home'
   get 'contact', to: 'pages#contact'
+  get 'imprint', to: 'pages#imprint'
 
   resources :contents
   resources :images, only: [:new, :create, :destroy, :index]
