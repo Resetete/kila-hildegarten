3d8912ee35f88a4545136d14827d4146ea5521bb
Theresa <tmannschatz@gmail.com>
2021-12-06 14:05:49 +0100

    Add all sections to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,6 @@
 class PagesController < ApplicationController
   def home
     # TODO: refactor --> maybe looping over array with page names
-    # TODO: improve page names
     # TODO: reduce image size --> image uploader
     @main_content = Content.find_by(page: 'home')
     @jobs = Content.find_by(page: 'open_positions')
@@ -9,9 +8,13 @@ class PagesController < ApplicationController
     @about_us = Content.find_by(page: 'about_us')
     @daily_life_intro = Content.find_by(page: 'daily_life_intro')
     @daily_life = Content.find_by(page: 'daily_life')
+    @rooms = Content.find_by(page: 'rooms')
+    @concept = Content.find_by(page: 'concept')
+    @familarization = Content.find_by(page: 'familarization')
 
-    @images = Image.where(page: 'home').last(Image::MAX_IMAGES_ON_HOMEPAGE) # max number of images shown on home page
+    @home_images = Image.where(page: 'home').last(Image::MAX_IMAGES_ON_HOMEPAGE) # max number of images shown on home page
     @daily_life_images = Image.where(page: 'daily_life').last(Image::MAX_IMAGES_ON_HOMEPAGE)
+    @rooms_images = Image.where(page: 'rooms')
   end
 
   def contact
