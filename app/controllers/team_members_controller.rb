083e49690221b8d6619fe030a52f3f0347437dfa
Theresa <tmannschatz@gmail.com>
2022-01-16 23:10:25 +0100

    Add team member index, new page with index form

--- app/controllers/team_members_controller.rb
+++ app/controllers/team_members_controller.rb
@@ -6,11 +6,12 @@ class TeamMembersController < ApplicationController
   def new
     @team_member = TeamMember.new
     @team_member.contents.build
+    @team_member.images.build
   end
 
   def create
     @team_member = TeamMember.new(team_member_params)
-    p "#{@team_member}"
+    @team_member.images.last.name = TeamMember.name
     if @team_member.save
       redirect_to team_members_path
     end
@@ -21,7 +22,8 @@ class TeamMembersController < ApplicationController
   def team_member_params
     params.require(:team_member).permit(
       :name,
-      contents_attributes: [:id, :page, :content]
+      contents_attributes: [:id, :page, :content],
+      images_attributes: [:id, :picture, :page]
     )
   end
 end
--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -2,5 +2,6 @@ class TeamMember < ApplicationRecord
   has_many :contents
   accepts_nested_attributes_for :contents #, allow_destroy: true
 
-  has_one :image
+  has_many :images
+  accepts_nested_attributes_for :images #, allow_destroy: true
 end
