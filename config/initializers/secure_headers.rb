fe620f009fb90672de1cfd7bb6fdb4bfe84454b1
Theresa <tmannschatz@gmail.com>
2025-10-05 22:31:10 +0200

    Fix CSP syntax and allow Google Fonts + inline JS

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -4,11 +4,11 @@ if Rails.env.production?
   SecureHeaders::Configuration.default do |config|
     config.csp = {
       default_src: %w('self'),
-      script_src: %w('self'),
+      script_src: %w('self' 'unsafe-inline'),
       connect_src: %w('self'),
       img_src: %w('self' data: https://*.dropboxusercontent.com),
-      style_src: %w('self' 'unsafe-inline' data: https://fonts.gstatic.com),
-      font_src: %w('self' data:),
+      style_src: %w('self' 'unsafe-inline' https://fonts.googleapis.com),
+      font_src: %w('self' data: https://fonts.gstatic.com),
       frame_ancestors: %w('self' https://hildegarten.webling.eu),
       form_action: %w('self'),
       base_uri: %w('self'),
