73dc4a77163b74b06075281bc537690c74f7a6f8
Theresa <tmannschatz@gmail.com>
2025-10-17 15:22:01 +0200

    comment scripts

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,7 @@
+require 'logger' unless defined?(::Logger::Severity)
+
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-require 'logger' unless defined?(::Logger::Severity)
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
 
