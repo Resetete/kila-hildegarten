ab8071bba835b16b7aae8c22d9051fa871c8951f
Theresa <tmannschatz@gmail.com>
2025-10-17 15:04:03 +0200

    Fix Heroku build: Webpacker + Logger + buildpacks

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,6 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-require 'logger' unless defined?(Logger)
+require 'logger' unless defined?(::Logger::Severity)
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+
